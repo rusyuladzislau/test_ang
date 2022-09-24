c1bbb5df6cf5cf86d5bce1d4726ab795326d0aa6
Uladzislau Rusy <work.rusyuladzislau@gmail.com>
2022-09-24 17:44:18 +0300

    fix: env stackbiz link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { TranslateService } from "@ngx-translate/core";
-import { environment } from "src/environments/environment";
+import { environment } from "../environments/environment";
 
 @Component({
   selector: "app-root",
